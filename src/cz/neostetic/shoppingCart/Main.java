ce80eed001cae3b3de3077e175d8f67b89d06289
jan.polacek <gg.polacek@gmail.com>
2021-09-23 15:04:02 +0200

    Project Add

---
+++ src/cz/neostetic/shoppingCart/Main.java
@@ -0,0 +1,57 @@
+package cz.neostetic.shoppingCart;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int price = 0;
+        for (String i : args) {
+            switch (i) {
+                case "0":
+                    System.out.println("Maslo");
+                    price += 999;
+                    break;
+                case "1":
+                    System.out.println("Rohliky");
+                    price += 5;
+                    break;
+                case "2":
+                    System.out.println("Pivo");
+                    price += 12;
+                    break;
+                case "3":
+                    System.out.println("Cokolada");
+                    price += 56;
+                    break;
+                case "4":
+                    System.out.println("Taska");
+                    price += 23;
+                    break;
+                case "5":
+                    System.out.println("Taska 2");
+                    price += 79;
+                    break;
+                case "6":
+                    System.out.println("Sunka");
+                    price += 23;
+                    break;
+                case "7":
+                    System.out.println("Paprika");
+                    price += 45;
+                    break;
+                case "8":
+                    System.out.println("Paprika 2");
+                    price += 73;
+                case "9":
+                    System.out.println("Chleba");
+                    price += 100;
+                    break;
+                default:
+                    break;
+            }
+            System.out.println("\n-------------------------\n"+price+" CZK");
+        }
+
+    }
+
+}
